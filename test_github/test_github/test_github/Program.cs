76ba310633becfa479c916b65ed79f62bd3e7b91
hejie <1462897949@qq.com>
2017-11-06 11:40:49 +0800

    test_github
    create a visual studio file about test_github

---
+++ test_github/test_github/test_github/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello github");
+            Console.ReadLine();
+
+        }
+    }
+}
